67860bda7e2abe3419bc4c90e842248615050472
Cheburusska <deminvictor@gmail.com>
2016-12-22 14:13:29 +0300

    mistakes fixed
    program don't crash if path is wrong
    comments fixed
    help added

--- pprint_json.py
+++ pprint_json.py
@@ -1,23 +1,29 @@
 import json
 from pprint import pprint
+import os.path
+import argparse
 
-json_path = 'json/alcohol.json' #path to the json file
 
-
-def load_data(filepath): #takes path to file, returns data in python format
+def load_data(filepath):
+    '''reads data from json file'''
+    if not os.path.exists(filepath):
+        return None
     with open(filepath, encoding="utf8") as json_file:
-        data = json.load(json_file)
-        return data
+        return json.load(json_file)
 
-
-def pretty_print_json(data): #takes data, prints to console in a readable format
+def pretty_print_json(data):
+    '''takes data, prints to console in a readable format'''
     pprint(data)
 
-
-def main():
-    data = load_data(json_path)
-    pretty_print_json(data)
-
-
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='Данная программа принимает от пользователя путь к файлу json и выводит содержимое файла в консоль в читаемом виде')
+    args = parser.parse_args()
+
+    while True:
+        json_path = input("Укажите путь к существующему файлу json: ")
+        data = load_data(json_path)
+        if data is not None:
+            break
+
+    pretty_print_json(data)
