baadc9cafa3f451b711dd13e622e3cad2541d3d5
Cheburusska <deminvictor@gmail.com>
2016-12-27 14:13:45 +0300

    new updations
    changed format of pretty print output
    added filepath as argument of argparse
    deleted excess comments

--- pprint_json.py
+++ pprint_json.py
@@ -1,28 +1,24 @@
 import json
-from pprint import pprint
-import os.path
-import argparse
+import os, argparse
 
 
 def load_data(filepath):
-    '''reads data from json file'''
     if not os.path.exists(filepath):
         return None
     with open(filepath, encoding="utf8") as json_file:
         return json.load(json_file)
 
 def pretty_print_json(data_from_json):
-    '''takes data, prints to console in a readable format'''
-    pprint(data_from_json)
+    print (json.dumps(data_from_json, indent=4, sort_keys=True))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Данная программа принимает от пользователя путь к файлу json и выводит содержимое файла в консоль в читаемом виде')
+    parser.add_argument('filepath', help='укажите файл в формате json')
     args = parser.parse_args()
 
     while True:
-        json_path = input("Укажите путь к существующему файлу json: ")
-        data_from_json = load_data(json_path)
+        data_from_json = load_data(args.filepath)
         if data_from_json is not None:
             break
 
